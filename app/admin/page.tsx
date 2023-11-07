fd81beac21231b3bbe3f26d6bfc564cef72601a6
JefersonCopini <116360705+JefersonCopini@users.noreply.github.com>
2023-11-07 23:46:38 +0000

    fix:Courses and admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,12 @@
 export default function Admin(){
-    return(
-        <h1>Area do adm</h1>
+    return (
+        <div>
+        <h1 className="text-center text-xl">Área Administrativa</h1>
+        <a href="/admin/course">Listar Curso</a>
+
+    
+
+        </div>
+        
     )
 }
