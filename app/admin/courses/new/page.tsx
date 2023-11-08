a777f27b2d4aff14a3773008540567b1002ec7d1
JefersonCopini <116360705+JefersonCopini@users.noreply.github.com>
2023-11-08 01:14:49 +0000

    fix: next

--- app/admin/courses/new/page.tsx
+++ app/admin/courses/new/page.tsx
@@ -1,12 +1,17 @@
 export default function NewCourse(){
+    function saveCourse(){
+        "use server"
+        console.log("Acessou a função")
+    }
     return(
         <div>
             <h1 className="text-white text-center text-4xl"> Cadastrar curso</h1>
             <form>
-                <input type="text" placeholder="Digite o titulo do curso " /> <br></br>
-                <input type="text" placeholder="Digite a Descrição do curso " /><br></br>
-                <input type="text" placeholder="Digite a url do curso " /><br></br>
-                <button className="text-white"> Salvar</button>
+                <input type="text" name="title" placeholder="Digite o titulo do curso " /> <br></br>
+                <input type="text" name="description" placeholder="Digite a Descrição do curso " /><br></br>
+                <input type="text" name="url" placeholder="Digite a url do curso " /><br></br>
+                <button formAction={saveCourse} className="text-white"> Salvar</button>
+
             </form>
         </div>
     )
